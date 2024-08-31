427de883d832573bc58055278cd0c23252fb63f5
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2024-08-31 07:19:46 +0200

    Update ft_strdup.c

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,26 +6,26 @@
 /*   By: csauron <csauron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/22 05:59:20 by csauron           #+#    #+#             */
-/*   Updated: 2024/08/31 04:09:52 by csauron          ###   ########.fr       */
+/*   Updated: 2024/08/31 07:19:45 by csauron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bsq.h"
 
-char    *ft_strdup(char *src)
+char	*ft_strdup(char *src)
 {
-    char    *ptr;
-    char    *dest;
+	char	*ptr;
+	char	*dest;
 
-    ptr = src;
-    while (*ptr)
-        ptr++;
-    dest = (char *)malloc((ptr - src + 1) * sizeof(char));
-    if(!dest)
-        return (NULL);
-    ptr = dest;
-    while (*src)
-        *(ptr++) = *(src++);
-    *ptr = '\0';
-    return (dest);
+	ptr = src;
+	while (*ptr)
+	ptr++;
+	dest = (char *)malloc((ptr - src + 1) * sizeof(char));
+	if (!dest)
+		return (NULL);
+	ptr = dest;
+	while (*src)
+	*(ptr++) = *(src++);
+	*ptr = '\0';
+	return (dest);
 }
