a407745958a3386f1513d213644ce1c7180e5079
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2024-08-31 07:26:21 +0200

    Update ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,25 +6,25 @@
 /*   By: csauron <csauron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 20:10:54 by csauron           #+#    #+#             */
-/*   Updated: 2024/08/31 04:54:13 by csauron          ###   ########.fr       */
+/*   Updated: 2024/08/31 07:26:21 by csauron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bsq.h"
 
-char    *ft_strjoin(char *s1, char *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
-    char    *dest;
-    char    *ptr;
+	char	*dest;
+	char	*ptr;
 
-    dest = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
-    if(!dest)
-        return (NULL);
-    ptr = dest;
-    while (*s1)
-        *(ptr++) = *(s1++);
-    while (*s2)
-        *(ptr++) = *(s2++);
-    *ptr = '\0';
-    return (dest);
+	dest = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!dest)
+		return (NULL);
+	ptr = dest;
+	while (*s1)
+	*(ptr++) = *(s1++);
+	while (*s2)
+	*(ptr++) = *(s2++);
+	*ptr = '\0';
+	return (dest);
 }
